e4ac89c8aae83b3f3f84fe6782333e47a841d0fb
AbileneM <abileneahue@gmail.com>
2024-11-15 22:41:20 -0500

    Add project files.

---
+++ WpfMvvmUA2/Views/Membres.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+
+
+namespace WpfMvvmUA2.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour Membres.xaml
+    /// </summary>
+    public partial class Membres : Page
+    {
+        public Membres()
+        {
+            InitializeComponent();
+        }
+    }
+}
