0b24a999cae2a229f525bc41cdd1485350710645
AbileneM <abileneahue@gmail.com>
2024-11-16 15:24:31 -0500

    Premier commit

---
+++ WpfMvvmUA2/Models/Membre.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMvvmUA2.Models
+{
+    public class Membre
+    { 
+        public Membre() { } required 
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class MembreList
+    {
+        public static ObservableCollection<Membre> _ListeMembre = new ObservableCollection<Membre>()
+        { };
+
+        public static ObservableCollection<Membre> GetMembres()
+        {
+            return _ListeMembre;
+        }
+        public static void AjouterMembre(Membre membre)
+        {
+            _ListeMembre.Add(membre);
+        }
+    }
+}
---
+++ WpfMvvmUA2/Models/Projet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMvvmUA2.Models
+{
+    public class Projet
+    {
+        public Projet() { }
+        public string Titre { get; set; }
+        public int Avancement { get; set; }
+
+    }
+
+    public class ProjetList
+    {
+        public static ObservableCollection<Projet> _ListeProjet = new ObservableCollection<Projet>()
+        { };
+
+        public static ObservableCollection<Projet> GetProjets()
+        {
+            return _ListeProjet;
+        }
+        public static void AjouterProjet(Projet projet)
+        {
+            _ListeProjet.Add(projet);
+        }
+    }
+}
